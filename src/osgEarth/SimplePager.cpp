bb8febc40ce9b827f269b202d9570c73ed9ff535
Jason Beverage <jasonbeverage@gmail.com>
2021-06-10 15:33:01 -0400

    Fixed bug in SimplePager. hasChildren should not be dependant on whether or not the parent tile had children or not.  This was causing graphs based on SimplePager to incorrectly subdivide and miss tiles depending on what the contents of the tiles were.

--- src/osgEarth/SimplePager.cpp
+++ src/osgEarth/SimplePager.cpp
@@ -112,7 +112,6 @@ SimplePager::createPagedNode(const TileKey& key, ProgressCallback* progress)
     if (key.getLOD() >= _minLevel)
     {
         node = createNode(key, progress);
-        hasChildren = node.valid();
     }
 
     osg::ref_ptr<PagedNode2> pagedNode = new PagedNode2();
@@ -123,10 +122,6 @@ SimplePager::createPagedNode(const TileKey& key, ProgressCallback* progress)
         pagedNode->addChild(node);
         fire_onCreateNode(key, node.get());
     }
-
-    tileRadius = osg::maximum(
-        tileBounds.radius(),
-        static_cast<osg::BoundingSphere::value_type>(tileRadius));
 
     pagedNode->setCenter(tileBounds.center());
     pagedNode->setRadius(tileRadius);
@@ -175,7 +170,6 @@ SimplePager::createPagedNode(const TileKey& key, ProgressCallback* progress)
                 return result;
             }
         );
-
         loadRange = (float)(tileRadius * _rangeFactor);
         pagedNode->setRefinePolicy(_additive ? REFINE_ADD : REFINE_REPLACE);
     }
